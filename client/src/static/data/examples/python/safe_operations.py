0c85f5f960baa8234bf3d6690296c876f7da4c53
Simone Lazzaris <slazzaris@gmail.com>
2021-03-11 15:03:45 +0100

    More verbose example comments

--- client/src/static/data/examples/python/safe_operations.py
+++ client/src/static/data/examples/python/safe_operations.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from immudb.client import ImmudbClient
 
 ic=ImmudbClient()
@@ -12,17 +10,22 @@ value="a_very_important_value".encode('utf8')
 # if it was correctly inserted
 response=ic.verifiedSet(key,value)
 
-# here response is a structure holding many informations
-# about the merkele tree, but the most important is that 
-# the insert was correctly verified
+# If database state after the insertion is consistent with the state we had before,
+# and the new value hash is included in new state, the transaction is verified.
 assert response.verified==True
 
 print("Key inserted (and verified) with index",response.id)
 
-#reads back the value
+# Now we read back the value we just stored
 readback=ic.verifiedGet(key)
 
-# in the readback we also have the index and the verified field
+# If the hash of the key/value is included in the database cryptographic state, 
+# the data is consistent and it is not been tampered. 
 assert response.verified==True
+
+# In the response we also have additional fields, such as the transaction id
+# and the unix epoch timestamp of the insertion,, which are also 
+# cryptographically verified.
+
 print("The value is {} at transaction {} with timestamp".
     format(readback.value.decode('utf8'),response.id,readback.timestamp))
