8af0b8ddbfd865683fa0ec34709e2276bbe1dbf1
StefanoSalidu <st.salidu@gmail.com>
2021-05-06 09:53:44 +0200

    chore: managed immudb expired token

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -14,6 +14,8 @@ const URL_WHITELISTED = [];
  */
 const PAYLOAD_MESSAGES_WHITELISTED = [];
 
+const PLEASE_LOGIN_FIRST = 'please login first';
+
 const VERBOSE = false;
 
 export default ({ store }) => {
@@ -33,13 +35,18 @@ export default ({ store }) => {
 					const { status: code, config, data } = err.response;
 					const url = config && config.url;
 					const error = data ? (data.error + '').toLowerCase() : undefined;
+					const { message } = data;
 
 					const IS_UNHAUTORIZED_ERROR = code === 401 || code === 403;
+					const IS_INTERNAL_SERVER_ERROR = code === 500;
 					const RETRY_REQUEST = config && config.__isRetryRequest;
 					const WHITELISTED = URL_WHITELISTED.some(x => url.includes(x)) ||
 						PAYLOAD_MESSAGES_WHITELISTED.some(x => error && error.includes(x));
 
 					if (IS_UNHAUTORIZED_ERROR && !RETRY_REQUEST && !WHITELISTED) {
+						store.commit(`${ IMMUDB_MODULE }/${ SET_TOKEN }`, null);
+					}
+					else if (IS_INTERNAL_SERVER_ERROR && message === PLEASE_LOGIN_FIRST) {
 						store.commit(`${ IMMUDB_MODULE }/${ SET_TOKEN }`, null);
 					}
 
