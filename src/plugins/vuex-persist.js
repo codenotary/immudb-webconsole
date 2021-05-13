888943876bbc8fd3d76c0adf2ae5bdab37729d57
StefanoSalidu <st.salidu@gmail.com>
2021-05-13 10:15:36 +0200

    feat: persisting pane sizes, sidebar preferences and timezone

--- src/plugins/vuex-persist.js
+++ src/plugins/vuex-persist.js
@@ -11,7 +11,10 @@ export default ({ store }) => {
 		reducer: state => ({
 			view: {
 				theme: state.view.theme,
+				sidebar: state.view.sidebar,
+				paneSizes: state.view.paneSizes,
 				hideDisabled: state.view.hideDisabled,
+				timezone: state.view.timezone,
 			},
 			auth: {
 				token: state.auth.token,
