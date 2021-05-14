828a2950d1c3f6edf063a81e6eafd3db04770f38
StefanoSalidu <st.salidu@gmail.com>
2021-05-14 15:39:24 +0200

    chore: updated logo components

---
+++ src/components/ui/logo/CodeNotary.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="ma-0 mb-4 pa-0 d-flex flex-column justify-center align-start fill-width">
+		<v-img
+			class="immudb"
+			:src="getAsset"
+			:alt="$t('common.codenotary')"
+			:height="`${ height }px`"
+			contain
+		/>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'UiLogoCodeNotary',
+	props: {
+		icon: { type: Boolean, default: false },
+		svg: { type: Boolean, default: true },
+		height: { type: [String, Number], default: 48 },
+	},
+	computed: {
+		getTheme () {
+			return this.$vuetify.theme.dark ? 'dark' : 'light';
+		},
+		getMime () {
+			return this.svg ? 'svg' : 'png';
+		},
+		getAsset () {
+			return require(`@/static/images/codenotary/logo_${ this.getTheme }${ (this.icon ? '_icon' : '') }.${ this.getMime }`);
+		},
+	},
+};
+</script>
---
+++ src/components/ui/logo/Immudb.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="ma-0 mb-4 pa-0 d-flex flex-column justify-center align-start fill-width">
+		<v-img
+			class="immudb"
+			:src="getAsset"
+			:alt="$t('common.immudb')"
+			:height="`${ height }px`"
+			contain
+			rel="preload"
+		/>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'UiPoweredBy',
+	props: {
+		svg: { type: Boolean, default: true },
+		height: { type: [String, Number], default: 48 },
+	},
+	computed: {
+		getTheme () {
+			return this.$vuetify.theme.dark ? 'dark' : 'light';
+		},
+		getMime () {
+			return this.svg ? 'svg' : 'png';
+		},
+		getAsset () {
+			return require(`@/static/images/immudb/text_${ this.getTheme }.${ this.getMime }`);
+		},
+	},
+};
+</script>
