0c20b2cf2d0419cc4af3fb474f3fc2d9ee1746d3
StefanoSalidu <st.salidu@gmail.com>
2021-06-24 11:33:07 +0200

    chore: updated demo data

--- src/components/query/tables/index.vue
+++ src/components/query/tables/index.vue
@@ -313,15 +313,15 @@ export default {
 
 				/* eslint-disable quotes */
 				// create and fill customers table with random data
-				await this.runSqlExec("CREATE TABLE customers (id INTEGER, customer_name VARCHAR, email VARCHAR, address VARCHAR, city VARCHAR, ip VARCHAR, country VARCHAR, PRIMARY KEY id);");
-				await this.runSqlExec("UPSERT INTO customers (id, customer_name, email, address, city, ip, country) values (1, 'Isidro Behnen', 'ibehnen0@mail.ru', 'ibehnen0@chronoengine.com', 'Arvika', '2.124.67.107', 'SE');");
-				await this.runSqlExec("UPSERT INTO customers (id, customer_name, email, address, city, ip, country) values (2, 'Claudianus Boldt', 'cboldt1@adobe.com', 'cboldt1@elpais.com', 'Kimhae', '125.89.31.130', 'KR');");
-				await this.runSqlExec("UPSERT INTO customers (id, customer_name, email, address, city, ip, country) values (3, 'Ginny Kelshaw', 'gkelshaw2@bbb.org', 'gkelshaw2@123-reg.co.uk', 'Nova Lima', '231.174.212.41', 'BR');");
-				await this.runSqlExec("UPSERT INTO customers (id, customer_name, email, address, city, ip, country) values (4, 'Paola Elverston', 'pelverston3@ox.ac.uk', 'pelverston3@cdc.gov', 'Morales', '114.130.229.206', 'GT');");
-				await this.runSqlExec("UPSERT INTO customers (id, customer_name, email, address, city, ip, country) values (1, 'Asa Antrum', 'aantrum0@apple.com', 'aantrum0@prweb.com', 'Selkirk', '193.5.161.248', 'CA');");
-				await this.runSqlExec("UPSERT INTO customers (id, customer_name, email, address, city, ip, country) values (2, 'Neila Rowena', 'nrowena1@google.ca', 'nrowena1@netscape.com', 'Liangdong', '65.163.125.159', 'CN');");
-				await this.runSqlExec("UPSERT INTO customers (id, customer_name, email, address, city, ip, country) values (3, 'Rex Massinger', 'rmassinger2@weibo.com', 'rmassinger2@jiathis.com', 'Jesús Menéndez', '165.100.211.115', 'CU');");
-				await this.runSqlExec("UPSERT INTO customers (id, customer_name, email, address, city, ip, country) values (4, 'Whit Shilton', 'wshilton3@amazonaws.com', 'wshilton3@cdbaby.com', 'Bobowo', '167.156.190.237', 'PL');");
+				await this.runSqlExec("CREATE TABLE customers (id INTEGER, customer_name VARCHAR, email VARCHAR, address VARCHAR, city VARCHAR, ip VARCHAR, country VARCHAR, age INTEGER, active BOOLEAN, PRIMARY KEY id);");
+				await this.runSqlExec("UPSERT INTO customers (id, customer_name, email, address, city, ip, country, age, active) values (1, 'Isidro Behnen', 'ibehnen0@mail.ru', 'ibehnen0@chronoengine.com', 'Arvika', '2.124.67.107', 'SE', 24 true);");
+				await this.runSqlExec("UPSERT INTO customers (id, customer_name, email, address, city, ip, country, age, active) values (2, 'Claudianus Boldt', 'cboldt1@adobe.com', 'cboldt1@elpais.com', 'Kimhae', '125.89.31.130', 'KR', 30, true);");
+				await this.runSqlExec("UPSERT INTO customers (id, customer_name, email, address, city, ip, country, age, active) values (3, 'Ginny Kelshaw', 'gkelshaw2@bbb.org', 'gkelshaw2@123-reg.co.uk', 'Nova Lima', '231.174.212.41', 'BR', 32, true);");
+				await this.runSqlExec("UPSERT INTO customers (id, customer_name, email, address, city, ip, country, age, active) values (4, 'Paola Elverston', 'pelverston3@ox.ac.uk', 'pelverston3@cdc.gov', 'Morales', '114.130.229.206', 'GT', 33, true);");
+				await this.runSqlExec("UPSERT INTO customers (id, customer_name, email, address, city, ip, country, age, active) values (1, 'Asa Antrum', 'aantrum0@apple.com', 'aantrum0@prweb.com', 'Selkirk', '193.5.161.248', 'CA', 46, true);");
+				await this.runSqlExec("UPSERT INTO customers (id, customer_name, email, address, city, ip, country, age, active) values (2, 'Neila Rowena', 'nrowena1@google.ca', 'nrowena1@netscape.com', 'Liangdong', '65.163.125.159', 'CN', 39, true);");
+				await this.runSqlExec("UPSERT INTO customers (id, customer_name, email, address, city, ip, country, age, active) values (3, 'Rex Massinger', 'rmassinger2@weibo.com', 'rmassinger2@jiathis.com', 'Jesús Menéndez', '165.100.211.115', 'CU', 28, false);");
+				await this.runSqlExec("UPSERT INTO customers (id, customer_name, email, address, city, ip, country, age, active) values (4, 'Whit Shilton', 'wshilton3@amazonaws.com', 'wshilton3@cdbaby.com', 'Bobowo', '167.156.190.237', 'PL', 52, false);");
 
 				// create products table with random data
 				await this.runSqlExec("CREATE TABLE products (id INTEGER, product VARCHAR, price VARCHAR, PRIMARY KEY id);");
