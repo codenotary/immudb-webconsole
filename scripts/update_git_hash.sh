aa154da97032a42f424fdb1db7128903c746ef29
Hextar13 <ste13456@gmail.com>
2021-09-14 16:36:59 +0200

    chore: updated hash script

--- scripts/update_git_hash.sh
+++ scripts/update_git_hash.sh
@@ -1,6 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 touch .env
 file=.env
-grep -q '^VUE_APP_GIT_COMMIT_HASH=' $file || echo 'VUE_APP_GIT_COMMIT_HASH=VALUE' >> $file
-sed -i "/VUE_APP_GIT_COMMIT_HASH/c\VUE_APP_GIT_COMMIT_HASH=$(git rev-parse --short HEAD)" $file
+
+if [ -f /.dockerenv ]; then
+    echo "I'm inside matrix ;(";
+	sed -i "/VUE_APP_GIT_COMMIT_HASH/c\VUE_APP_GIT_COMMIT_HASH=$WEBCONSOLE_VERSION" $file
+else
+	grep -q '^VUE_APP_GIT_COMMIT_HASH=' $file || echo 'VUE_APP_GIT_COMMIT_HASH=VALUE' >> $file
+	sed -i "/VUE_APP_GIT_COMMIT_HASH/c\VUE_APP_GIT_COMMIT_HASH=$(git rev-parse --short HEAD)" $file
+fi
