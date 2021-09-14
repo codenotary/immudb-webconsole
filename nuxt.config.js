115a78c2ca0c61e2ef2198e76a78640949d23c76
Hextar13 <ste13456@gmail.com>
2021-09-14 13:12:43 +0200

    chore: removed purgecss

--- nuxt.config.js
+++ nuxt.config.js
@@ -166,19 +166,6 @@ export default {
 				['@babel/plugin-proposal-private-methods', { loose: true }],
 			],
 		},
-		// postcss: {
-		// 	plugins: {
-		// 		'postcss-url': false,
-		// 		'postcss-nested': {},
-		// 		'postcss-responsive-type': {},
-		// 		'postcss-hexrgba': {},
-		// 	},
-		// 	preset: {
-		// 		autoprefixer: {
-		// 			grid: true,
-		// 		},
-		// 	},
-		// },
 	},
 
 	/*
