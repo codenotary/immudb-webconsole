17d4ce2dff0a1cc479675913b01371f84ea9dfbf
Hextar13 <ste13456@gmail.com>
2021-09-22 11:28:14 +0200

    chore: updated nuxt and disabled experimental build/generate features

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import Sass from 'sass';
 const SITE_NAME = 'immudb webconsole';
 const IS_PROD = process.env.NODE_ENV === 'production';
 const IS_PUBLIC_DEMO = process.env.PUBLIC_DEMO;
-const EXPERIMENTAL = true;
+const EXPERIMENTAL = false;
 
 console.log('=======================================');
 console.log(`Running ${ IS_PUBLIC_DEMO
